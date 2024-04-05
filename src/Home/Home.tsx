8716e46b8f4da38ab66dbeae4b1b3ac66b13431e
HarshaR1642 <sriharsha@rently.com>
2024-04-06 02:04:26 +0530

    remove animation

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,38 +1,20 @@
 import React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
-import Animated, { useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
+import { StyleSheet, View } from "react-native";
+import { Label } from "../common/components";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
 
 export const HomeTab = () => {
-    const alpha = useSharedValue(0);
-
-    React.useEffect(() => {
-        alpha.value = withRepeat(withTiming(
-            Dimensions.get('window').height - 100,
-            {
-                duration: 1000
-            }
-        ), -1, true)
-    }, [])
 
     return (
-        <View style={{ flex: 1, alignItems: 'center' }}>
-            <Animated.View
-                style={{
-                    top: alpha,
-                    width: 100,
-                    height: 100,
-                    borderRadius: 10,
-                    elevation: 5,
-                    backgroundColor: 'red',
-                }}
-            />
+        <View style={styles.container}>
+            <Label xxl primary title="Home" />
         </View>
     )
 
